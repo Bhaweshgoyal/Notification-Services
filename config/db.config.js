3e17fec17d26aa69681336405efa89f31092d720
Bhawesh Goyal <105268824+Bhaweshgoyal@users.noreply.github.com>
2023-04-24 08:18:41 +0530

    db updated

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
-const notificationServiceDbUri = "mongodb://127.0.0.1:27017/notificationServiceDb";
+const notificationServiceDbUri = "mongodb+srv://bhaweshg0777:RJqSUUWVgkuHcApK@notificationservice.f89ceep.mongodb.net/?retryWrites=true&w=majority";
 
 module.exports = {db_uri : notificationServiceDbUri};
